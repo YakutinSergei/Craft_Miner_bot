2cb89374a9abfdcfbae36fe44a748abb03eeb439
YakutinSergei <yakutinserg@yandex.ru>
2023-11-18 12:09:08 +0300

    sfd

---
+++ handlers/deposit_handler.py
@@ -0,0 +1,19 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message
+
+from bot import bot
+from bot_menu.menu import create_kb_menu
+from data_bases.orm_basic import add_user, get_user
+#from keyboards.user_kb import create_kb_menu
+from lexicon.lexicon_ru import LEXICON_PROFILE, LEXICON_MINES
+
+router: Router = Router()
+
+
+@router.message(F.text == [LEXICON_MINES['natural_gas'], LEXICON_MINES['uranium'], LEXICON_MINES['coal'],
+                           LEXICON_MINES['oil'], LEXICON_MINES['gold']])
+async def mines(message: Message):
+    print('nnnnn')
