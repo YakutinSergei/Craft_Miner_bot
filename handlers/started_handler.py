488f6f4ad340ffc28a3bcef56a8d8a0c08b912fc
YakutinSergei <yakutinserg@yandex.ru>
2023-11-18 11:54:50 +0300

    sdf

--- handlers/started_handler.py
+++ handlers/started_handler.py
@@ -24,7 +24,7 @@ async def process_start_command(message: Message, state: FSMContext):
     if user:
         await message.answer(text="<b><i><u>ПРОФИЛЬ</u></i></b>\n"
                                   f"<b><u>Баланс: </u></b> {user[0]['balance']}{LEXICON_PROFILE['price']}\n"
-                                  f"<b><u>Заполненность склада:</u></b> {user[1]['sum']/user['volume_stock']*100} %\n"
+                                  f"<b><u>Заполненность склада:</u></b> {int(user[1]['sum'])/int(user['volume_stock'])*100} %\n"
                                   f"<b><u>Рейтинг:</u></b> {user[0]['rating']}", reply_markup=await create_kb_menu(user['name_deposit']))
     else:
         await message.answer(text='Приветствую тебя в игре!\n'
