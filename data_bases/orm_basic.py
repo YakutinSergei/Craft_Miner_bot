0f871f3151574851afeafba93f106e00f07c5154
YakutinSergei <97688943+YakutinSergei@users.noreply.github.com>
2023-11-19 15:38:34 +0300

    пар

--- data_bases/orm_basic.py
+++ data_bases/orm_basic.py
@@ -36,7 +36,7 @@ async def get_user(tg_id:int):
 
         #Обновление склада
         await conn.execute(f'''UPDATE user_deposits
-                                SET stock = stock + ((uw.lvl * w.efficiency * uw.sum) * EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - u.date))::int)
+                                SET stock = stock + ((w.efficiency * uw.sum) * EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - u.date)/60)::int)
                                 FROM user_workers uw
                                 JOIN workers w ON uw.id_worker = w.id_worker
                                 JOIN users u ON u.id_user = uw.id_user
